5ac68dbc2533677da9a7816d66cef059e28d2dff
crazyyy <crazyyy@gmail.com>
2015-07-13 17:50:53 +0300

    script

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,43 @@
-!function(){for(var e,n=function(){},o=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeline","timelineEnd","timeStamp","trace","warn"],i=o.length,r=window.console=window.console||{};i--;)e=o[i],r[e]||(r[e]=n)}();
+
+
+var apiUrl = 'content/vn-locations.json';
+
+function loadMarkers() {
+  $.getJSON(apiUrl, function(data) {
+    var i = 0,
+      dataName = [],
+      dataLtLg = [],
+      dataFull = [];
+    for( i=0; i<data.length; i++ ){
+      dataName.push(data[i].name),
+      dataLtLg.push(data[i].latlng),
+      dataFull.push( '{' + 'latlng: ' + data[i].latlng + ',' + 'name: ' + data[i].name + '}' );
+    };
+
+    console.log(dataFull);
+
+    $('#map').kendoMap({
+      center: [49.21596533483571, 28.40863362665015],
+      zoom: 14,
+      layers: [{
+        type: 'tile',
+        urlTemplate: 'http://#= subdomain #.tile2.opencyclemap.org/transport/#= zoom #/#= x #/#= y #.png',
+        subdomains: ['a', 'b', 'c'],
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap contributors</a>.' + 'Tiles courtesy of <a href="http://www.opencyclemap.org/">Andy Allan</a>'
+      },
+      {
+        type: 'marker',
+        dataSource: {
+          data: [{
+            latlng: dataFull,
+            name : dataFull
+          }]
+        },
+        locationField: 'latlng',
+        titleField: 'name'
+      }]
+    });
+  });
+};
+
+$(document).ready(loadMarkers);
