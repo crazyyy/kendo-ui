4e7cc330567413b57239ab8291ca6d68236de18b
crazyyy <crazyyy@gmail.com>
2015-07-17 15:00:36 +0300

    array fixed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,38 +5,63 @@ var apiUrl = 'content/vn-locations.json';
 function loadMarkers() {
   $.getJSON(apiUrl, function(data) {
     var i = 0,
-      dataName = [],
-      dataLtLg = [],
-      dataFull = [];
+    dataFull = [];
+
     for( i=0; i<data.length; i++ ){
-      dataName.push(data[i].name),
-      dataLtLg.push(data[i].latlng),
-      dataFull.push( '{' + 'latlng: ' + data[i].latlng + ',' + 'name: ' + data[i].name + '}' );
+      dataFull.push({
+        name : data[i].name,
+        latlng : data[i].latlng,
+      });
     };
 
-    console.log(dataFull);
+    var dl = new kendo.data.DataSource({
+      data: dataFull
+    });
 
-    $('#map').kendoMap({
-      center: [49.21596533483571, 28.40863362665015],
-      zoom: 14,
+    console.log(dl);
+
+    $("#map").kendoMap({
+        center: [49.22623440491784,28.446919692124307],
+        zoom: 14,
       layers: [{
         type: 'tile',
         urlTemplate: 'http://#= subdomain #.tile2.opencyclemap.org/transport/#= zoom #/#= x #/#= y #.png',
         subdomains: ['a', 'b', 'c'],
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap contributors</a>.' + 'Tiles courtesy of <a href="http://www.opencyclemap.org/">Andy Allan</a>'
-      },
-      {
-        type: 'marker',
-        dataSource: {
-          data: [{
-            latlng: dataFull,
-            name : dataFull
-          }]
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap contributors</a>.' + 'Tiles courtesy of <a href="http://www.opencyclemap.org/">Andy Allan</a>',
         },
-        locationField: 'latlng',
-        titleField: 'name'
-      }]
+        {
+          type: "marker",
+          dataSource: dl,
+          locationField: "latlng",
+          titleField: 'name'
+        }
+
+        ]
     });
+
+
+
+    // $('#map').kendoMap({
+    //   center: [49.22623440491784,28.446919692124307],
+    //   zoom: 14,
+    //   layers: [{
+    //     type: 'tile',
+    //     urlTemplate: 'http://#= subdomain #.tile2.opencyclemap.org/transport/#= zoom #/#= x #/#= y #.png',
+    //     subdomains: ['a', 'b', 'c'],
+    //     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap contributors</a>.' + 'Tiles courtesy of <a href="http://www.opencyclemap.org/">Andy Allan</a>'
+    //   },
+    //   {
+    //     type: 'marker',
+    //     dataSource: {
+    //       data: [{
+    //         latlng: [[49.22623440491784,28.446919692124307], [49.219627001532366,28.42785904275598]] ,
+    //         name : ['Ashanti', 'Иль-де-Франс / ILe de France'],
+    //       }]
+    //     },
+    //     locationField: 'latlng',
+    //     titleField: 'name'
+    //   }]
+    // });
   });
 };
 
